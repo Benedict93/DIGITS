ac9329a6b59941d6a4c35090375efac2fe5660b0
Benedict <benedictchensiyuan@hotmail.com>
2018-04-11 13:08:02 +0800

    s

--- digits/tools/pytorch/main.py
+++ digits/tools/pytorch/main.py
@@ -254,7 +254,7 @@ def main():
 
     logging.info('Started training the model')
     save = 0
-    
+
     for epoch in range(1, args.epoch+1):
         if args.snapshotInterval > 0 and current_epoch >= next_snapshot_save:
             save = 1
@@ -291,6 +291,8 @@ def train(epoch, model, train_loader, optimizer, save):
         if save == 1:
             save_state = {'epoch': epoch, 'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
             torch.save (save_state, args.save)
+            snapshot_file = os.path.join(args.save, snapshot_prefix + '_' + epoch_fmt.format(epoch) + '.pt')
+            logging.info('Snapshotting to %s', snapshot_file)
 
         if batch_idx % log_interval == 0:
             print('Train Epoch: {}\t'
