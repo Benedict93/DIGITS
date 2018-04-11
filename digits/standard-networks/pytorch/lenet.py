adcd2921400987393e8ac9b5119df59f9fc084ad
Benedict <benedictchensiyuan@hotmail.com>
2018-04-11 21:14:01 +0800

    d

--- digits/standard-networks/pytorch/lenet.py
+++ digits/standard-networks/pytorch/lenet.py
@@ -17,7 +17,7 @@ class Net(nn.Module):
     F7 - 10 (Output)
     """
     def __init__(self):
-        super(LeNet5, self).__init__()
+        super(Net, self).__init__()
 
         self.convnet = nn.Sequential(OrderedDict([
             ('c1', nn.Conv2d(1, 6, kernel_size=(5, 5))),
