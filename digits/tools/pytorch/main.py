7538b2d02735743bea934cafaacec83824c5a729
Benedict <benedictchensiyuan@hotmail.com>
2018-04-13 21:13:34 +0800

    test

--- digits/tools/pytorch/main.py
+++ digits/tools/pytorch/main.py
@@ -152,27 +152,28 @@ def train(epoch, model, train_loader, optimizer, save, snapshot_prefix, snapshot
         loss.backward()
         optimizer.step()
 
-        if save == 1 and epoch.is_integer() and epoch != 0:
-            print('Saving snapshot')
-            save_state = {'epoch': epoch, 'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
-            number_dec = str(snapshot_interval-int(snapshot_interval))[2:]
-            if number_dec is '':
-                number_dec = '0'
-            epoch_fmt = "{:." + number_dec + "f}"
-            snapshot_file = os.path.join(args.save, snapshot_prefix + '_' + epoch_fmt.format(epoch) + '.pth.tar')
-            logging.info('Snapshotting to %s', snapshot_file)
-            torch.save (save_state, snapshot_file)
-            logging.info('Snapshot saved.')
-
         if batch_idx % log_interval == 0:
             epoch += 0.1
             if epoch.is_integer() or epoch == 0:
                 epoch = intial_epoch
+            if save == 1 and epoch.is_integer() and epoch != 0:
+                print('Saving snapshot')
+                save_state = {'epoch': epoch, 'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+                number_dec = str(snapshot_interval-int(snapshot_interval))[2:]
+                if number_dec is '':
+                    number_dec = '0'
+                epoch_fmt = "{:." + number_dec + "f}"
+                snapshot_file = os.path.join(args.save, snapshot_prefix + '_' + epoch_fmt.format(epoch) + '.pth.tar')
+                logging.info('Snapshotting to %s', snapshot_file)
+                torch.save (save_state, snapshot_file)
+                logging.info('Snapshot saved.')
             print('Train Epoch: {}\t'
                  'Batch: [{:5d}/{:5d} ({:3.0f}%)]\t'
                  'Loss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset), 
                     100. * batch_idx / len(train_loader), losses.val))
             logging.info("Training (epoch " + str(epoch) + "):" + " loss = " + str(losses.val) + ", lr = " + str(args.lr_base_rate) + ", accuracy = {0:.2f}".format(accuracy.avg))
+
+    
 
 
 
